3bcc12e547262c0d6bb9ec394eb5e0346f607175
Dev Naina <67383307+MdNaina@users.noreply.github.com>
2023-06-19 21:00:20 +0530

    Dev (#1) add embedded binary
    * FIX: release action
    
    * FIX: action 'yarn' -> 'npm'
    
    * UPDATE: add embedded executable
    
    add embedded executable to eleminate manual installation
    
    ---------
    
    Co-authored-by: mdnaina <mdnaina@pop-os.localdomain>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,35 +1,58 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::process::Command;
+use tauri::api::process::{Command as Cmd, CommandEvent};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn generate_backup(url: &str, folder: &str, db: &str) -> String {
-    let cmd = Command::new("mongodump")
-        .args(["--uri", url])
-        .arg(format!("--db={}", db))
-        .args(["-o", folder])
-        // .arg(format!("--archive={}/{}.db", folder, db))
-        // .arg("--gzip")
-        .output()
-        .expect("cannot open");
-    format!("DB log:\n{}", String::from_utf8_lossy(&cmd.stderr))
+fn generate_backup(window: tauri::Window, url: &str, folder: &str) -> String {
+    let cmd = Cmd::new_sidecar("db-dump").unwrap();
+    dbg!(&cmd);
+    let cmd = cmd.args([url, "-o", folder.clone()]);
+    dbg!(&cmd);
+    let (mut rx, mut child) = cmd.spawn().unwrap();
+
+    tauri::async_runtime::spawn(async move {
+        // read events such as stdout
+        while let Some(event) = rx.recv().await {
+            dbg!(&event);
+            if let CommandEvent::Stderr(line) = event {
+                dbg!(line);
+                // write to stdin
+                child.write("message from Rust\n".as_bytes()).unwrap();
+            }
+        }
+        window
+            .emit("status", format!("your db has backuped"))
+            .unwrap();
+        println!("run once");
+    });
+    format!("Done")
 }
 
 #[tauri::command]
-fn restore_db(url: &str, db: &str, path: &str) -> String {
-    let cmd = Command::new("mongorestore")
-        .arg(url)
-        .arg(format!("--nsInclude={}", db))
-        // .arg(format!("--archive={}", path))
-        .arg(path)
-        .output()
-        .expect("cannot open");
-    format!(
-        "DB log {}:\n {}",
-        &cmd.status,
-        String::from_utf8_lossy(&cmd.stderr)
-    )
+fn restore_db(window: tauri::Window, url: &str, path: &str) -> String {
+    let cmd = Cmd::new_sidecar("db-restore").unwrap();
+    dbg!(&cmd);
+    let cmd = cmd.args([url, path]);
+    dbg!(&cmd);
+    let (mut rx, mut child) = cmd.spawn().unwrap();
+
+    tauri::async_runtime::spawn(async move {
+        // read events such as stdout
+        while let Some(event) = rx.recv().await {
+            dbg!(&event);
+            if let CommandEvent::Stderr(line) = event {
+                dbg!(line);
+                // write to stdin
+                child.write("message from Rust\n".as_bytes()).unwrap();
+            }
+        }
+        window
+            .emit("status", format!("your db has restored"))
+            .unwrap();
+        println!("run once");
+    });
+    format!("Done")
 }
 
 fn main() {
