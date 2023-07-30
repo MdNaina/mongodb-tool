b11d6643675117152a0e2a0ca313f2343f744043
MdNaina <mdnaina@2kdeveloper.com>
2023-07-30 13:28:12 +0530

    FIX: binary install script

--- scripts/install-embed-binaries.cjs
+++ scripts/install-embed-binaries.cjs
@@ -5,7 +5,36 @@ const path = require("path");
 const decompress = require('decompress');
 const decompressTargz = require('decompress-targz');
 
+const VERSION = "100.7.2"
+
+const platformUrlObj = {
+  "windows-latest": "windows-x86_64",
+  "ubuntu-20.04": "ubuntu2004-x86_64",
+  "macos-latest": "macos-x86_64"
+}
+console.log(process.argv)
 const platformName = process.argv.at(2)
+
+console.log(platformUrlObj[platformName], platformName)
+if (!platformUrlObj[platformName]) {
+  console.error(`platform incorrect please select from this (${Object.keys(platformUrlObj).join(",")})`)
+  process.exit(1)
+}
+// Specify the URL of the compressed file
+const url = `https://fastdl.mongodb.org/tools/db/mongodb-database-tools-${platformUrlObj[platformName]}-${VERSION}.${platformName === "ubuntu-20.04" ? "tgz" : "zip"}`;
+
+// Specify the target path to save the compressed file
+const tempDir = path.join(os.tmpdir(), "mongodb-tools")
+const distDir = path.join(process.cwd(), "src-tauri", "binaries")
+
+if (!fs.existsSync(tempDir))
+  fs.mkdirSync(tempDir, { recursive: true })
+
+if (!fs.existsSync(distDir))
+  fs.mkdirSync(distDir, { recursive: true })
+
+const targetPath = path.join(tempDir, "archive");
+
 const suffixObj = {
   "windows-latest": "x86_64-pc-windows-msvc",
   "ubuntu-20.04": "x86_64-unknown-linux-gnu",
@@ -36,11 +65,12 @@ const downloadAndExtractFiles = async (url, targetPath) => {
 
     // Move extracted files to the desired location and delete the extraction-temp folder
     fs.readdirSync(extractionTempPath, { recursive: true }).forEach(file => {
-      const sourcePath = `${extractionTempPath}/${file}`;
+      const sourcePath = path.join(extractionTempPath, file);
       const { name, ext } = path.parse(file)
       console.log({ name })
       if (requireProgram[name]) {
-        const destinationPath = `${process.cwd()}/src-tauri/binaries/${requireProgram[name]}-${suffixObj[platformName]}${ext}`;
+        path.join()
+        const destinationPath = path.join(distDir, `${requireProgram[name]}-${suffixObj[platformName]}${ext}`);
         fs.renameSync(sourcePath, destinationPath);
       }
     });
@@ -50,35 +80,6 @@ const downloadAndExtractFiles = async (url, targetPath) => {
     console.error('Error:', error.message);
   }
 };
-
-const VERSION = "100.7.2"
-
-const platformUrlObj = {
-  "windows-latest": "windows-x86_64",
-  "ubuntu-20.04": "ubuntu2004-x86_64",
-  "macos-latest": "windows-x86_64"
-}
-console.log(process.argv)
-
-console.log(platformUrlObj[platformName], platformName)
-if (!platformUrlObj[platformName]) {
-  console.error(`platform incorrect please select from this (${Object.keys(platformUrlObj).join(",")})`)
-  process.exit(1)
-}
-// Specify the URL of the compressed file
-const url = `https://fastdl.mongodb.org/tools/db/mongodb-database-tools-${platformUrlObj[platformName]}-${VERSION}.${platformName === "ubuntu-20.04" ? "tgz" : "zip"}`;
-
-// Specify the target path to save the compressed file
-const tempDir = `${os.tmpdir()}/mongodb-tools`
-const distDir = `${process.cwd()}/src-tauri/binaries`
-
-if (!fs.existsSync(tempDir))
-  fs.mkdirSync(tempDir, { recursive: true })
-
-if (!fs.existsSync(distDir))
-  fs.mkdirSync(distDir, { recursive: true })
-
-const targetPath = `${tempDir}/archive`;
 
 
 // Call the function to download and extract the files
@@ -94,6 +95,3 @@ downloadAndExtractFiles(url, targetPath).then(() => {
     }
   });
 });
-
-
-
